bd6c62bc4f13662d8c00114f5d2a1abaa2381915
hongxichen <hxc@hongxichendemacbook-pro.local>
2016-04-08 15:51:59 +0800

    Initial Commit

---
+++ draw/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  draw
+//
+//  Created by 洪曦尘 on 16/4/8.
+//  Copyright © 2016年 洪曦尘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ draw/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  draw
+//
+//  Created by 洪曦尘 on 16/4/8.
+//  Copyright © 2016年 洪曦尘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
